87ccfa6bc1ccbee01fbc01fcf1d9cbcaf0bf417e
Henry Corredor González <78055527+henrycorredor7570@users.noreply.github.com>
2023-12-30 09:09:51 -0500

    add abstract methods

--- src/main/java/mx/com/gm/servicio/PersonaService.java
+++ src/main/java/mx/com/gm/servicio/PersonaService.java
@@ -3,7 +3,7 @@ package mx.com.gm.servicio;
 import java.util.List;
 import mx.com.gm.domain.Persona;
 
-public interface PersonaService {
+public interface PersonaService{
     
     public List<Persona> listarPersonas();
     
