751a9e3a14eb674e807a8b149e2bb099e9ee0fd0
Henry Corredor González <78055527+henrycorredor7570@users.noreply.github.com>
2024-05-10 16:14:47 -0500

    Update EncriptarPassword.java

--- src/main/java/mx/com/gm/util/EncriptarPassword.java
+++ src/main/java/mx/com/gm/util/EncriptarPassword.java
@@ -5,7 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class EncriptarPassword {
     public static void main(String[] args) {
         var password = "123";
-        System.out.println("Password sin encriptar = " + password);
+        System.out.println("Password sin encriptar = " + password); 
         System.out.println("Password encriptada = " + encriptarPassword(password));        
     }
     
